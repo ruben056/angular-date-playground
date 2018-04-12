729a7d715067face9329f6e47e30a4c9a630945f
ruben056 <demuynck_ruben@hotmail.com>
2018-04-12 21:14:10 +0200

    Made datepicker editable for manual userinput. When clicking UP the picker pops up and you can choose the date.
    TODO usability improvements
    - tab is now prevented because tabbing out of the input otherwise opened the datepicker
    don't know the idea behind this but it sux as default behaviour. Need to find way to disable this but focus should go to next object correctly...
    - focus after closing date should go to input element
    - ...??- ...????

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -3,42 +3,58 @@ import {MaterializeAction} from 'angular2-materialize/dist/materialize-directive
 import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
 declare var Materialize: any;
 
+const KEYCODE_TAB :number = 9;
+const KEYCODE_UP :number = 38;
+
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
   styleUrls: ['./date-picker.component.css']
 })
+
 export class DatePickerComponent {
 
-  birthDate:string;
-    birthTime:string;
+    birthDate:string;
+    otherDate:string;
 
     birthDateActions = new EventEmitter<string|MaterializeAction>();
-    birthTimeActions = new EventEmitter<string|MaterializeAction>();
+    otherDateActions = new EventEmitter<string|MaterializeAction>();
     form: FormGroup;
-
 
     constructor(private fb: FormBuilder) {
         this.birthDate = "03/12/2017";
-        this.birthTime = "12:36";
-        this.form = this.fb.group({
-            'fromDate': new FormControl('06/07/2017'),
-            'fromTime': new FormControl('08:30')
-        });
+        this.otherDate = "04/12/2017";
     }
 
-    openDatePicker() {
-        //actions are open or close
-        this.birthDateActions.emit({action: "pickadate", params: ["open"]});
+    birthDateKeyPressed($event:any){
+      console.log($event);
+      this.keyPressedOnDateInput($event, this.birthDateActions)
     }
 
-    setTime(time) {
-        this.birthTime = time;
+    otherDateKeyPressed($event:any){
+      console.log($event);
+      this.keyPressedOnDateInput($event, this.otherDateActions)
     }
 
-    openTimePicker() {
-        //actions are show or hide
-        this.birthTimeActions.emit({action: "pickatime", params: ["show"]});
-}
+    keyPressedOnDateInput($event, actionsEmitter: EventEmitter<string|MaterializeAction>){
+      if($event.keyCode == KEYCODE_UP){
+        this.startDatePicker(actionsEmitter);
+        this.openDatePicker(actionsEmitter);
+      }
+      else if($event.keyCode == KEYCODE_TAB){
+        $event.preventDefault();
+        // $event.stopPropagation();
+      }
+    }
+
+    stopDatePicker(actionsEmitter: EventEmitter<string|MaterializeAction>){
+      actionsEmitter.emit({action: "pickadate", params: ["stop"]});
+    }
+    startDatePicker(actionsEmitter: EventEmitter<string|MaterializeAction>){
+      actionsEmitter.emit({action: "pickadate", params: ["start"]});
+    }
+    openDatePicker(actionsEmitter: EventEmitter<string|MaterializeAction>) {
+      actionsEmitter.emit({action: "pickadate", params: ["open"]});
+    }
 
 }
